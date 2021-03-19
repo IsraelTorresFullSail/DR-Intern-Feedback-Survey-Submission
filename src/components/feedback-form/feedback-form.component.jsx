682dad0d4099aeb5755256366517715c5baf9711
Israel Torres <israeltorres@Israel-Mac-Mini.local>
2021-03-19 15:19:37 -0400

    first step implemented

--- src/components/feedback-form/feedback-form.component.jsx
+++ src/components/feedback-form/feedback-form.component.jsx
@@ -9,274 +9,314 @@ import FormStep4 from '../form-step4/form-step4.component';
 import FormStep5 from '../form-step5/form-step5.component';
 
 export class FeedbackForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            step: 1,
-            redirect: false,
-            firstName: '',
-            lastName: '',
-            companyName: '',
-            relationshipFeedback: '',
-            recommend: 'yes',
-            whyRecommend: '',
-            testimonial: '',
-            services: {},
-            formErrors: { firstName: '', lastName: '', companyName: '', relationshipFeedback: '', recommend: '', whyRecommend: '', testimonial: '', services: {} },
-            firstNameValid: false,
-            lastNameValid: false,
-            companyNameValid: false,
-            relationshipFeedbackValid: false,
-            // recommendValid: false,
-            whyRecommendValid: false,
-            testimonialValid: false,
-            // servicesValid: false,
-            formValid: false,
-            copied: false,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 1,
+      redirect: false,
+      firstName: '',
+      lastName: '',
+      semester_year: '',
+      duration: '',
+      teams_worked: {},
+      favorite_part: '',
+      skills_gain: '',
+      improve_program: '',
+      recommend_program: '',
+      formErrors: {
+        firstName: '',
+        lastName: '',
+        semester_year: '',
+        duration: '',
+        teams_worked: {},
+        favorite_part: '',
+        skills_gain: '',
+        improve_program: '',
+        recommend_program: '',
+      },
+      firstNameValid: false,
+      lastNameValid: false,
+      semester_yearValid: false,
+      durationValid: false,
+      favorite_partValid: false,
+      skills_gainValid: false,
+      improve_programValid: false,
+      recommend_programValid: false,
+      formValid: false,
+    };
+  }
+
+  // Proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1,
+    });
+  };
+
+  // Go back to prev step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1,
+    });
+  };
+
+  // Handle fields change
+  handleChange = (input) => (e) => {
+    e.persist();
+    this.setState({ [input]: e.target.value }, () => {
+      this.validateField(input, e.target.value);
+    });
+  };
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let firstNameValid = this.state.firstNameValid;
+    let lastNameValid = this.state.lastNameValid;
+    let semester_yearValid = this.state.semester_yearValid;
+    let durationValid = this.state.durationValid;
+    let favorite_partValid = this.state.favorite_partValid;
+    let skills_gainValid = this.state.skills_gainValid;
+    let improve_programValid = this.state.improve_programValid;
+    let recommend_programValid = this.state.recommend_programValid;
+
+    switch (fieldName) {
+      case 'firstName':
+        firstNameValid = value.length >= 2;
+        fieldValidationErrors.firstName = firstNameValid
+          ? ''
+          : ' is empty or too short';
+        break;
+      case 'lastName':
+        lastNameValid = value.length >= 2;
+        fieldValidationErrors.lastName = lastNameValid
+          ? ''
+          : ' is empty or too short';
+        break;
+      case 'semester_year':
+        semester_yearValid = value.length >= 2;
+        fieldValidationErrors.semester_year = semester_yearValid
+          ? ''
+          : ' is empty or too short';
+        break;
+      case 'duration':
+        durationValid = value.length >= 2;
+        fieldValidationErrors.duration = durationValid
+          ? ''
+          : ' is empty or too short';
+        break;
+      case 'favorite_part':
+        favorite_partValid = value.length >= 2;
+        fieldValidationErrors.favorite_part = favorite_partValid
+          ? ''
+          : ' is empty or too short';
+        break;
+      case 'skills_gain':
+        skills_gainValid = value.length >= 2;
+        fieldValidationErrors.skills_gain = skills_gainValid
+          ? ''
+          : ' is empty or too short';
+        break;
+      case 'improve_program':
+        improve_programValid = value.length >= 2;
+        fieldValidationErrors.improve_program = improve_programValid
+          ? ''
+          : ' is empty or too short';
+        break;
+      case 'recommend_program':
+        recommend_programValid = value.length >= 2;
+        fieldValidationErrors.recommend_program = recommend_programValid
+          ? ''
+          : ' is empty or too short';
+        break;
+      default:
+        break;
     }
+    this.setState(
+      {
+        formErrors: fieldValidationErrors,
+        firstNameValid: firstNameValid,
+        lastNameValid: lastNameValid,
+        semester_yearValid: semester_yearValid,
+        durationValid: durationValid,
+        favorite_partValid: favorite_partValid,
+        skills_gainValid: skills_gainValid,
+        improve_programValid: improve_programValid,
+        recommend_programValid: recommend_programValid,
+      },
+      this.validateForm
+    );
+  }
 
-    // Proceed to next step
-    nextStep = () => {
-        const { step } = this.state;
-        this.setState({
-            step: step + 1
-        })
-    }
+  validateForm() {
+    this.setState({
+      formValid:
+        this.state.firstNameValid &&
+        this.state.lastNameValid &&
+        this.state.semester_yearValid &&
+        this.state.durationValid &&
+        this.state.favorite_partValid &&
+        this.state.skills_gainValid &&
+        this.state.improve_programValid &&
+        this.state.recommend_programValid,
+    });
+  }
 
-    // Go back to prev step
-    prevStep = () => {
-        const { step } = this.state;
-        this.setState({
-            step: step - 1
-        })
-    }
+  // Handle select input
+  handleChangeDuration = (durationSelected) => {
+    this.setState({ duration: durationSelected });
+  };
 
-    // Handle fields change
-    handleChange = input => e => {
-        e.persist()
-        this.setState({ [input]: e.target.value }, () => {
-            this.validateField(input, e.target.value);
-        });
-    }
+  // Handle checkbox change
+  handleCheckboxChange = (e) => {
+    const { name } = e.target;
+    this.setState((previousState) => {
+      const teams_worked = { ...previousState.teams_worked };
+      teams_worked[name] = !teams_worked[name];
+      return { teams_worked };
+    });
+  };
 
-    validateField(fieldName, value) {
-        let fieldValidationErrors = this.state.formErrors;
-        let firstNameValid = this.state.firstNameValid;
-        let lastNameValid = this.state.lastNameValid;
-        let companyNameValid = this.state.companyNameValid;
-        let relationshipFeedbackValid = this.state.relationshipFeedbackValid;
-        // let recommendValid = this.state.recommendValid;
-        let whyRecommendValid = this.state.whyRecommendValid;
-        let testimonialValid = this.state.testimonialValid;
-        // let servicesValid = this.state.servicesValid;
-    
-        switch (fieldName) {
-          case 'firstName':
-            firstNameValid = value.length >= 2;
-            fieldValidationErrors.firstName = firstNameValid ? '' : ' is empty or too short';
-            break;
-          case 'lastName':
-            lastNameValid = value.length >= 2;
-            fieldValidationErrors.lastName = lastNameValid ? '' : ' is empty or too short';
-            break;
-          case 'companyName':
-            companyNameValid = value.length >= 2;
-            fieldValidationErrors.companyName = companyNameValid
-              ? ''
-              : ' is empty or too short';
-            break;
-          case 'relationshipFeedback':
-            relationshipFeedbackValid = value.length >= 2;
-            fieldValidationErrors.relationshipFeedback = relationshipFeedbackValid ? '' : ' is empty or too short';
-            break;
-        // case 'recommend':
-        //     recommendValid = value.length >= 2;
-        //     fieldValidationErrors.recommend = recommendValid ? '' : ' is empty or too short';
-        //     break;
-        case 'whyRecommend':
-            whyRecommendValid = value.length >= 2;
-            fieldValidationErrors.whyRecommend = whyRecommendValid ? '' : ' is empty or too short';
-            break;
-        case 'testimonial':
-            testimonialValid = value.length >= 2;
-            fieldValidationErrors.testimonial = testimonialValid ? '' : ' is empty or too short';
-            break;
-        // case 'services':
-        //     servicesValid = value.length >= 1;
-        //     fieldValidationErrors.services = servicesValid ? '' : ' is empty';
-        //     break;
-          default:
-            break;
-        }
-        this.setState(
-          {
-            formErrors: fieldValidationErrors,
-            firstNameValid: firstNameValid,
-            lastNameValid: lastNameValid,
-            companyNameValid: companyNameValid,
-            relationshipFeedbackValid: relationshipFeedbackValid,
-            //recommendValid: recommendValid,
-            whyRecommendValid: whyRecommendValid,
-            testimonialValid: testimonialValid,
-            //servicesValid: servicesValid,
-          },
-          this.validateForm
-        );
-      }
-    
-      validateForm() {
-        this.setState({
-          formValid:
-            this.state.firstNameValid &&
-            this.state.lastNameValid &&
-            this.state.companyNameValid &&
-            this.state.relationshipFeedbackValid &&
-            //this.state.recommendValid &&
-            this.state.whyRecommendValid &&
-            this.state.testimonialValid 
-            //this.state.servicesValid,
-        });
-      }
-    
-      
+  // Handle submit
+  handleSubmit = (e) => {
+    const url = 'https://usebasin.com/f/af7afa0b5ce6';
 
-    // Handle checkbox change
-    handleCheckboxChange = e => {
-        const { name } = e.target;
-        this.setState( previousState => {
-            const services = { ...previousState.services }; 
-            services[name] = !services[name];
-            return { services };
-        });
-    }
+    const {
+      firstName,
+      lastName,
+      semester_year,
+      duration,
+      teams_worked,
+      favorite_part,
+      skills_gain,
+      improve_program,
+      recommend_program,
+    } = this.state;
 
-    // Handle submit 
-    handleSubmit = e => {
-        // const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-        const url = "https://usebasin.com/f/455b81f716b8";
+    const teamsArray = Object.keys(teams_worked);
+    const teamsReturn = teamsArray.join(', ');
 
-        const { 
-            firstName,
-            lastName,
-            companyName,
-            relationshipFeedback,
-            recommend,
-            whyRecommend,
-            testimonial,
-            services,
-        } = this.state;
+    let formData = new FormData();
+    formData.append('Subject', 'DR - Intern Feedback Survey Submission');
+    formData.append('Intern Info:', '');
+    formData.append('First Name:', firstName);
+    formData.append('Last Name:', lastName);
+    formData.append('Semester/Year of Internship:', semester_year);
+    formData.append('Duration of Internship:', duration);
+    formData.append('Feedback:', '');
+    formData.append('Teams Worked on During Internship:', teamsReturn);
+    formData.append(
+      'What was your favorite part of being an intern at Digital Resource?',
+      favorite_part
+    );
+    formData.append(
+      'What new skills, techniques, and knowledge did you gain during your internship?',
+      skills_gain
+    );
+    formData.append(
+      'How could we improve our program for future interns? :',
+      improve_program
+    );
+    formData.append(
+      'Would you recommend our internship program to a friend? Why or why not?:',
+      recommend_program
+    );
 
-        // Save testimonial to be copied in the next step
-        localStorage.setItem('testimonial', this.state.testimonial);
+    let request = new XMLHttpRequest();
+    request.open('POST', url);
+    request.send(formData);
 
-        const servicesArray = Object.keys(services);
-        const servicesReturn = servicesArray.join(", ");
+    this.setState({ redirect: true });
+  };
 
-        let formData = new FormData();
-        formData.append('Subject', 'DR - Clients Feedback');
-        formData.append('Client Info:', '');
-        formData.append('First Name:', firstName);
-        formData.append('Last Name:', lastName);
-        formData.append('Company Name:', companyName);
-        formData.append('What services the client receive:', servicesReturn);
-        formData.append('What the clients like about their experience with Digital Resource:', relationshipFeedback);
-        formData.append('Would the client recommend DR to a friend?', recommend);
-        formData.append('Why?', whyRecommend);
-        formData.append('Client Testimonial:', testimonial);
+  render() {
+    const { step, redirect } = this.state;
+    const {
+      firstName,
+      lastName,
+      semester_year,
+      duration,
+      teams_worked,
+      favorite_part,
+      skills_gain,
+      improve_program,
+      recommend_program,
+      formErrors,
+    } = this.state;
+    const values = {
+      firstName,
+      lastName,
+      semester_year,
+      duration,
+      teams_worked,
+      favorite_part,
+      skills_gain,
+      improve_program,
+      recommend_program,
+      formErrors,
+    };
 
-        let request = new XMLHttpRequest();
-        request.open("POST", url);
-        request.send(formData);
-
-        this.setState({ redirect: true });
-    }
-
-
-    render() {
-        const { step, redirect } = this.state;
-        const { 
-                firstName,
-                lastName,
-                companyName,
-                relationshipFeedback,
-                recommend,
-                whyRecommend,
-                testimonial,
-                services,
-                formErrors,
-                copied
-              } = this.state;
-        const values = {
-                        firstName,
-                        lastName,
-                        companyName,
-                        relationshipFeedback,
-                        recommend,
-                        whyRecommend,
-                        testimonial,
-                        services,
-                        formErrors,
-                        copied
-                    };
-
-        return redirect? ( <Redirect to="/success" /> ) : (
-            <div className="container">
-                {(() => {
-                    switch (step) {
-                        case 1:
-                            return (
-                                <FormStep1 
-                                    nextStep={this.nextStep}
-                                    handleChange={this.handleChange}
-                                    values={values}
-                                />
-                            );
-                        case 2:
-                            return (
-                                <FormStep2 
-                                    nextStep={this.nextStep}
-                                    prevStep={this.prevStep}
-                                    handleChange={this.handleCheckboxChange}
-                                    values={values}
-                                />
-                            );
-                        case 3:
-                            return (
-                                <FormStep3 
-                                    nextStep={this.nextStep}
-                                    prevStep={this.prevStep}
-                                    handleChange={this.handleChange}
-                                    values={values}
-                                />
-                            );
-                        case 4:
-                            return (
-                                <FormStep4 
-                                    nextStep={this.nextStep}
-                                    prevStep={this.prevStep}
-                                    handleChange={this.handleChange}
-                                    values={values}
-                                />
-                            );
-                        case 5:
-                            return (
-                                <FormStep5 
-                                    prevStep={this.prevStep}
-                                    handleSubmit={this.handleSubmit}
-                                    handleChange={this.handleChange}
-                                    values={values}
-                                    valid={this.state.formValid}
-                                    errors={this.state.formErrors}
-                                />
-                            );
-                        default:
-                            (console.log('This is a multi-step form built with React.'))
-                    }
-                })()}
-            </div>
-        )
-    }
+    return redirect ? (
+      <Redirect to="/success" />
+    ) : (
+      <div className="container">
+        {(() => {
+          switch (step) {
+            case 1:
+              return (
+                <FormStep1
+                  nextStep={this.nextStep}
+                  handleChange={this.handleChange}
+                  handleChangeDuration={this.handleChangeDuration}
+                  values={values}
+                />
+              );
+            case 2:
+              return (
+                <FormStep2
+                  nextStep={this.nextStep}
+                  prevStep={this.prevStep}
+                  handleChange={this.handleCheckboxChange}
+                  values={values}
+                />
+              );
+            case 3:
+              return (
+                <FormStep3
+                  nextStep={this.nextStep}
+                  prevStep={this.prevStep}
+                  handleChange={this.handleChange}
+                  values={values}
+                />
+              );
+            case 4:
+              return (
+                <FormStep4
+                  nextStep={this.nextStep}
+                  prevStep={this.prevStep}
+                  handleChange={this.handleChange}
+                  values={values}
+                />
+              );
+            case 5:
+              return (
+                <FormStep5
+                  prevStep={this.prevStep}
+                  handleSubmit={this.handleSubmit}
+                  handleChange={this.handleChange}
+                  values={values}
+                  valid={this.state.formValid}
+                  errors={this.state.formErrors}
+                />
+              );
+            default:
+              console.log('This is a multi-step form built with React.');
+          }
+        })()}
+      </div>
+    );
+  }
 }
 
 export default FeedbackForm;
--- src/components/form-step1/form-step1.component.jsx
+++ src/components/form-step1/form-step1.component.jsx
@@ -1,82 +1,110 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 import CustomButton from '../custom-button/custom-button.component';
 
 import './form-step1.styles.scss';
 
 export class FormStep1 extends Component {
-    
-    continue = e => {
-        e.preventDefault();
-        this.props.nextStep();
-    }
+  continue = (e) => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
 
-    errorClass(error) {
-        return error.length === 0 ? '' : 'has-error';
-    };
-    
-    render() {
-        const { values, handleChange } = this.props;
-        console.log()
-        return (
-            <div className="layer">
-                <div className="digital-name"></div>
-                <div className="white-box">
-                    <div className="logo"></div>
-                    <div className="form-container">
-                        <h1>Client Feedback</h1>
-                        <h2>Your continued success is very important to us!</h2>
-                        <div className="fields-wrapper">
-                            <TextField 
-                                type="text"
-                                name="firstName"
-                                defaultValue={values.firstName}
-                                className={`form__input ${this.errorClass(
-                                    values.formErrors.firstName
-                                )}`}
-                                id="firstName"
-                                label="First Name"
-                                required
-                                onChange={handleChange('firstName')}
-                            />
-                            <TextField 
-                                type="text"
-                                name="lastName"
-                                defaultValue={values.lastName}
-                                className={`form__input ${this.errorClass(
-                                    values.formErrors.lastName
-                                )}`}
-                                id="lastName"
-                                label="Last Name"
-                                required
-                                onChange={handleChange('lastName')}
-                            />
-                            <TextField 
-                                type="text"
-                                name="companyName"
-                                defaultValue={values.companyName}
-                                className={`form__input ${this.errorClass(
-                                    values.formErrors.companyName
-                                )}`}
-                                id="companyName"
-                                label="Company Name"
-                                required
-                                onChange={handleChange('companyName')}
-                            />
-                            <CustomButton onClick={this.continue}>Next</CustomButton>
-                            <div className="dots-wrapper">
-                                <div className="dot active"></div>
-                                <div className="dot"></div>
-                                <div className="dot"></div>
-                                <div className="dot"></div>
-                                <div className="dot"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  errorClass(error) {
+    return error.length === 0 ? '' : 'has-error';
+  }
+
+  render() {
+    const { values, handleChange, handleChangeDuration } = this.props;
+    console.log();
+    return (
+      <div className="layer">
+        <div className="digital-name"></div>
+        <div className="white-box">
+          <div className="logo"></div>
+          <div className="form-container">
+            <h1>Intern Feedback Survey</h1>
+            <h2>Thanks for all of your hard work!</h2>
+            <p className="p-ghost">
+              Fields with an <span className="required">*</span> are required
+            </p>
+            <div className="fields-wrapper">
+              <TextField
+                type="text"
+                name="firstName"
+                defaultValue={values.firstName}
+                className={`form__input ${this.errorClass(
+                  values.formErrors.firstName
+                )}`}
+                id="firstName"
+                label="First Name"
+                required
+                onChange={handleChange('firstName')}
+              />
+              <TextField
+                type="text"
+                name="lastName"
+                defaultValue={values.lastName}
+                className={`form__input ${this.errorClass(
+                  values.formErrors.lastName
+                )}`}
+                id="lastName"
+                label="Last Name"
+                required
+                onChange={handleChange('lastName')}
+              />
+              <TextField
+                type="text"
+                name="semester_year"
+                defaultValue={values.semester_year}
+                className={`form__input ${this.errorClass(
+                  values.formErrors.semester_year
+                )}`}
+                id="semester_year"
+                label="Semester/Year of Internship"
+                required
+                onChange={handleChange('semester_year')}
+              />
+              <FormControl
+                className={`select__input ${this.errorClass(
+                  values.formErrors.duration
+                )}`}
+              >
+                <InputLabel>Duration of Internship</InputLabel>
+                <Select
+                  name="duration"
+                  value={values.duration ? values.duration : ''}
+                  onChange={(e) => handleChangeDuration(e.target.value)}
+                  label="Duration of Internship"
+                >
+                  <MenuItem value="">
+                    <em>Duration of Internship</em>
+                  </MenuItem>
+                  <MenuItem value={'0-1 months'}>0-1 months</MenuItem>
+                  <MenuItem value={'2-3 months'}>2-3 months</MenuItem>
+                  <MenuItem value={'4-6 months'}>4-6 months</MenuItem>
+                  <MenuItem value={'6+ months'}>6+ months</MenuItem>
+                </Select>
+              </FormControl>
+              <CustomButton onClick={this.continue}>Next</CustomButton>
+              <div className="dots-wrapper">
+                <div className="dot active"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FormStep1;
